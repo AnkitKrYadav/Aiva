badfe4ea9257404f27d1c3012a6384c2e68da8c5
AnkitKrYadav <67411716+AnkitKrYadav@users.noreply.github.com>
2025-03-29 15:27:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,74 @@
-#modules needed are imported
-import os ,playsound, pyttsx3,subprocess as sp,speech_recognition as sr,sys #pip install playsound==1.2.2 SpeechRecognition pyttsx3 pyaudio pocketsphinx
-from google import genai #pip install -U google-genai
+'''Modules needed for stt,q processing,interacting with system to do work,tts are imported'''
+#Todo : Add chat system to remember prev talks,better speech recognition.
+
+import os, sys, subprocess as sp
+import pyttsx3, speech_recognition as sr     #pip install SpeechRecognition pyttsx3 pyaudio, pocketsphinx playsound==1.2.2
+from google import genai                     #pip install -U google-genai
 from google.genai import types
+from google.genai.types import Tool,GoogleSearch
 
 # Global variables
+name=''
+prompt=(f'You are Aiva,a reliable AI assistant known for providing concise,to-the-point answers in respect to user name {name} '
+       'from india, in max 120 words.Your tone is friendly, and always approachable.')
 api_key= os.getenv("GEMINI_API_KEY")
 api_key2= os.getenv("GEMINI_API_KEY2")
 client = genai.Client(api_key=api_key)
-name=''
+google_search_tool = Tool(
+    google_search = GoogleSearch()
+)
 
-#pyttsx initialisation
+#pyttsx initialization
 engine= pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
-#Functions to use repetitively
 
-def askai(q):#Process the user querry and provide output
-    global client 
-    try:   
+#Functions:
+def askai(q):
+    '''Provide output of query through G-api'''
+    global client,api_key,api_key2,name
+
+    try:
         response = client.models.generate_content(
         model="gemini-2.0-flash",    
-        config=types.GenerateContentConfig(system_instruction='You are Aiva a reliable ai assistant who provides consize and to the point answers as much as possible,and be soothing/friendly',
-        temperature=1),
+        config=types.GenerateContentConfig(
+        tools=[google_search_tool],
+        #response_modalities=["TEXT"],
+            system_instruction=prompt,
+            temperature=0.9),
         contents=q
         )
         return response.text.replace('*','')
+
     except Exception as e:
-        print('1: '+str(e))
-        try:            
+        print('1: '+str(e))#edit 1
+        try:
             client = genai.Client(api_key=api_key2)
             api_key,api_key2=api_key2,api_key
 
             response = client.models.generate_content(
-            model="gemini-2.0-flash",    
-            config=types.GenerateContentConfig(system_instruction='You are Aiva a reliable ai assistant who provides consize and to the point answers as much as possible,and be soothing/friendly.',
-            temperature=1),
+            model="gemini-2.0-flash",
+            config=types.GenerateContentConfig(
+                system_instruction=prompt,
+                temperature=0.9),
             contents=q
             )
             return response.text.replace('*','')
-        except Exception as e:
-            print('2: '+str(e))
+        except Exception as f:
+            print('2: '+str(f))
 
-def speak(txt):#text to speech
+def speak(txt):
+    '''convert text to speech'''
     engine.say(txt)
     print('AI   : '+txt+'\n')
     engine.runAndWait()    
 
-def tc():  #take_voice_command(speech to text)  
-    r,order=sr.Recognizer(),''
+def tc():  
+    '''Take_voice_command and return command in text format'''    
     #r.energy_threshold=50
     #r.pause_threshold=0.5
+    r,order=sr.Recognizer(),''
 
     with sr.Microphone() as source:        
         #r.adjust_for_ambient_noise(source,duration=0.5)
@@ -61,15 +79,16 @@ def tc():  #take_voice_command(speech to text)
             print('USER : '+ order)
 
         except Exception :
-           print("Exception: " + str(Exception)+'|| Unable to recognise your voice\n')         
+            print("Exception: " + str(Exception)+'|| Unable to recognize your voice\n')         
 
     return order
 
-def intro():#Greet user
+def intro():
+    '''Greet user with name'''
     speak('Hello sir, what\'s your name ?')
     global name
     name=tc().title()
-    speak(f'Nice to meet you {name}, i am aiva how can i help you today sir ?')
+    speak(f'Great to meet you {name}, i am aiva, how can i help you today sir ?')
 
 # Program starts here
 print('\n------------------------------------------------------------------------------------------------------------------')
@@ -113,14 +132,13 @@ while True:
             sp.call(['shutdown','/h'])
         else:
             print(order)
-            pass
 
     elif ('exit'==order):
         speak(f"Have a great day ahead, good bye {name}.")
         sys.exit()
 
     elif (order==''):
-     pass
+        pass
 
     else:       
         speak(askai(order))
